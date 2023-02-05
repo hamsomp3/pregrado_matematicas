2b5da721168b44d5ebfb3f1977abc594a1f3110b
Jan Polanco V <44455794+hamsomp3@users.noreply.github.com>
2023-02-04 23:12:25 -0500

    4th version - algorithms

---
+++ 02_Algoritmos/03_SGD/stoch_grad_descent.m
@@ -0,0 +1,80 @@
+function [xopt,fopt,niter,gnorm,dx] = stoch_grad_descent(varargin)
+% stoch_grad_descent.m demonstrates how the stochastic gradient descent method can be used
+% to solve a simple unconstrained optimization problem.In stochastic
+% gradient descent, we randomly pick a variable and compute the gradient.
+%
+% Author: Paras Babu Tiwari, Washington University in Saint Louis;
+% http://www.cse.wustl.edu/~tiwarip/
+% This program is modification of the gradient descent written by James T. Allison, Assistant Professor, University of Illinois at
+% Urbana-Champaign
+% Date: 09/24/2013
+if nargin==0
+    % define starting point
+    x0 = [5 5]';
+elseif nargin==1
+    % if a single input argument is provided, it is a user-defined starting
+    % point.
+    x0 = varargin{1};
+else
+    error('Incorrect number of input arguments.')
+end
+tol     = 1e-6; % termination tolerance
+maxiter = 1000; % maximum number of allowed iterations
+dxmin   = 1e-6; % minimum allowed perturbation
+alpha   = 0.1;  % step size ( 0.33 causes instability, 0.2 quite accurate)
+gnorm   = inf;  % initialize gradient norm
+x       = x0;   % optimization vector
+niter   = 0;    % Iteration counter, perturbation
+dx      = inf;  % Perturbation
+
+% define the objective function:
+f = @(x1,x2) x1.^2 + x1.*x2 + 3*x2.^2;
+% plot objective function contours for visualization:
+figure(1); clf; fcontour(f,[-5 5 -5 5]); axis equal; hold on
+
+
+% redefine objective function syntax for use with optimization:
+f2 = @(x) f(x(1),x(2));
+h = get(gca, 'xlabel');
+set(h,'FontSize',22);
+h = get(gca, 'ylabel');
+set(h,'FontSize',22);
+h=get(gca,'Title');
+set(h,'FontSize',22);
+set(gca,'FontSize',15);
+% gradient descent algorithm:
+while and(gnorm>=tol, and(niter <= maxiter, dx >= dxmin))
+    % calculate gradient:
+    r = randi([1 2]);
+    g = grad(x,r);
+    gnorm = norm(g);
+    % take step:
+    xnew = x - alpha*g;
+    % check step
+    if ~isfinite(xnew)
+        display(['Number of iterations: ' num2str(niter)])
+        error('x is inf or NaN')
+    end
+    % plot current point
+    plot([x(1) xnew(1)],[x(2) xnew(2)],'ko-')
+    refresh
+    % update termination metrics
+    niter = niter + 1;
+    dx = norm(xnew-x);
+    x = xnew;
+    
+end
+xopt = x;
+fopt = f2(xopt);
+niter = niter - 1;
+%saveas(gcf, '~/grad_descent/stoch_fig', 'jpg');
+%fid = fopen('~/grad_descent/stoc_output.txt','w');
+%fprintf(fid,'x:%f\n',x);
+%fprintf(fid,'f:%f \n # of Iterations:%d\n',fopt,niter);
+%fclose(fid);
+% define the gradient of the objective
+function g = grad(x,r)
+grd = [2*x(1) + x(2)
+    x(1) + 6*x(2)];
+g = zeros(size(x,1),1);
+g(r) = grd(r);
